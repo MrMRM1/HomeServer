26077cce91dcd8c0dd1915457d93e7eb29dcb03d
MrMRM1 <9mehrabani@gmail.com>
2022-06-28 22:19:20 +0430

    feat: avoid running two versions of the program on Linux at the same time

--- scripts/already_running.py
+++ scripts/already_running.py
@@ -23,11 +23,31 @@ def windows_kill_process(id_process):
     return pop.stdout
 
 
+def linux_get_process():
+    app_name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+    cmd = "ps -x"
+    pop = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    ids = []
+    for i in pop.stdout:
+        data = i.decode().rstrip().split(' ')
+        if os.path.splitext(os.path.basename(data[-1]))[0] == app_name:
+            ids.append(data[2])
+    return ids
+
+
+def linux_kill_process(id_process):
+    cmd = f'kill {id_process}'
+    pop = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    return pop.stdout
+
+
 def get_process():
     data = None
     match platform.system():
         case 'Windows':
             data = windows_get_process()
+        case 'Linux':
+            data = linux_get_process()
         case _:
             pass
     return data
@@ -70,6 +90,8 @@ class SingleInstance(object):
         match platform.system():
             case 'Windows':
                 data = windows_kill_process(id_process)
+            case 'Linux':
+                data = linux_kill_process(id_process)
             case _:
                 pass
         self.update()
