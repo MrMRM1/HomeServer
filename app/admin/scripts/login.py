c246c6573f328f975bd58633e11914a4dd178714
MrMRM1 <9mehrabani@gmail.com>
2022-07-19 15:25:03 +0430

    feat: add error_login and user_login

--- app/admin/scripts/login.py
+++ app/admin/scripts/login.py
@@ -1,8 +1,10 @@
 from functools import wraps
 
-from flask_login import login_required
+from flask_login import login_required, login_user
+from flask import render_template, redirect, request
 
 from app.scripts.sqllite import database
+from .user import User
 
 
 def login_required_custom(func):
@@ -15,3 +17,16 @@ def login_required_custom(func):
             return func(*args, **kwargs)
 
     return decorated_view
+
+
+def error_login(guest_status):
+    alert = 'Username or password is incorrect.'
+    guest = False
+    if guest_status == '1':
+        guest = True
+    return render_template('login.html', guest=guest, alert=alert)
+
+
+def user_login(username):
+    login_user(User(username))
+    return redirect(request.args['next'])
