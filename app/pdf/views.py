eabf3b95460793581de08d05e4f1ec54e0ce777f
MrMRM1 <9mehrabani@gmail.com>
2022-07-19 15:36:44 +0430

    feat: add login_required_custom for routes

--- app/pdf/views.py
+++ app/pdf/views.py
@@ -2,20 +2,24 @@ from flask import render_template
 
 from . import pdf
 from scripts.paths import list_dir, list_file, check_dir_flask
+from app.admin.scripts.login import login_required_custom
 
 
 @pdf.route('/pdf')
+@login_required_custom
 def list_folders():
     return render_template("list_folders.html", title="List Folders", items=','.join(list_dir()), typs='pdf')
 
 
 @pdf.route('/pdf/<path:link>')
+@login_required_custom
 @check_dir_flask
 def list_pdf(link):
     return render_template("list_folders.html", title=link, items=list_file(['pdf'], link), typs="show_pdf")
 
 
 @pdf.route('/show_pdf/<path:link>')
+@login_required_custom
 @check_dir_flask
 def show_pdf(link):
     return render_template('viewer.html', title=link.split('/')[-1], link=link)
