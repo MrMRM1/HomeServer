3df913dff01b71ffe06eb583a12c2af2436ef48c
MrMRM1 <9mehrabani@gmail.com>
2022-07-19 11:10:45 +0430

    update

--- app/admin/scripts/login.py
+++ app/admin/scripts/login.py
@@ -1,11 +1,10 @@
-from app.scripts.sqllite import Database
+from app.scripts.sqllite import database
 from flask_login import login_required
 
 
 def login_required_custom(func):
 
     def decorated_view(*args, **kwargs):
-        database = Database()
         if database.get_data()[11] == '1':
             return login_required(func)(*args, **kwargs)
         else:
