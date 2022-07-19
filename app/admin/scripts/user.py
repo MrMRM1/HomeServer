f423e9e78329006c8161c6ad6bd271baeb6f40a3
MrMRM1 <9mehrabani@gmail.com>
2022-07-19 11:10:22 +0430

    feat: add User class

---
+++ app/admin/scripts/user.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self, username):
+        self.username = username
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.username
