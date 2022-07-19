ed322c12648a8279ce36c36c61652eda5e4ec545
MrMRM1 <9mehrabani@gmail.com>
2022-07-19 15:37:25 +0430

    feat: add login_required_custom for routes

--- app/picture/views.py
+++ app/picture/views.py
@@ -2,14 +2,17 @@ from flask import render_template
 
 from . import picture
 from scripts.paths import list_dir, list_file, check_dir_flask
+from app.admin.scripts.login import login_required_custom
 
 
 @picture.route('/picture')
+@login_required_custom
 def list_folders():
     return render_template("list_folders.html", title="List Folders", items=','.join(list_dir()), typs='picture')
 
 
 @picture.route('/picture/<path:link>')
+@login_required_custom
 @check_dir_flask
 def picture_page(link):
     return render_template("picture.html", title=link,
