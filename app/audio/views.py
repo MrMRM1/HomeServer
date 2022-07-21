3ed242b3d8a9ab97502a9982d90ce4936e8c2ade
MrMRM1 <9mehrabani@gmail.com>
2022-07-21 10:15:15 +0430

    add access_status

--- app/audio/views.py
+++ app/audio/views.py
@@ -2,18 +2,20 @@ from flask import render_template
 
 from . import audio
 from scripts.paths import list_dir, list_file, check_dir_flask
-from app.admin.scripts.login import login_required_custom
+from app.admin.scripts.login import login_required_custom, access_status
 
 
 @audio.route('/audio')
 @login_required_custom
 def audio_page():
-    return render_template("list_folders.html", title="List Folders", items=','.join(list_dir()), typs="audio")
+    if access_status(6):
+        return render_template("list_folders.html", title="List Folders", items=','.join(list_dir()), typs="audio")
 
 
 @audio.route('/audio/<path:link>')
 @login_required_custom
 @check_dir_flask
 def list_audios(link):
-    return render_template("list_audios.html", title=link, items=list_file(['mp3', 'wav', 'ogg'], link),
-                           typs="show_audio")
+    if access_status(6):
+        return render_template("list_audios.html", title=link, items=list_file(['mp3', 'wav', 'ogg'], link),
+                               typs="show_audio")
