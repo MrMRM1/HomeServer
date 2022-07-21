0d410e2279ff38e2ff11697ee4ef219adfc135e2
MrMRM1 <9mehrabani@gmail.com>
2022-07-21 10:31:40 +0430

    add access_status

--- app/picture/views.py
+++ app/picture/views.py
@@ -2,19 +2,21 @@ from flask import render_template
 
 from . import picture
 from scripts.paths import list_dir, list_file, check_dir_flask
-from app.admin.scripts.login import login_required_custom
+from app.admin.scripts.login import login_required_custom, access_status
 
 
 @picture.route('/picture')
 @login_required_custom
 def list_folders():
-    return render_template("list_folders.html", title="List Folders", items=','.join(list_dir()), typs='picture')
+    if access_status(11):
+        return render_template("list_folders.html", title="List Folders", items=','.join(list_dir()), typs='picture')
 
 
 @picture.route('/picture/<path:link>')
 @login_required_custom
 @check_dir_flask
 def picture_page(link):
-    return render_template("picture.html", title=link,
-                           items=list_file(['apng', 'gif', 'ico', 'cur', 'jpg', 'jpeg', 'jfif', 'pjpeg', 'pjp',
-                                            'png', 'png'], link), typs="show_picture")
+    if access_status(11):
+        return render_template("picture.html", title=link,
+                               items=list_file(['apng', 'gif', 'ico', 'cur', 'jpg', 'jpeg', 'jfif', 'pjpeg', 'pjp',
+                                                'png', 'png'], link), typs="show_picture")
