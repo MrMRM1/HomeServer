0d9e2cd98e6a7cded6c9263ac6dedc9ec75ee968
MrMRM1 <9mehrabani@gmail.com>
2022-07-23 14:54:48 +0430

    feat: create and send a secret to display the video

--- app/video/views.py
+++ app/video/views.py
@@ -1,8 +1,13 @@
-from flask import render_template, abort
+import json
+import time
+
+from flask import render_template, request, jsonify
+from flask_login import current_user
 
 from . import video
-from app.scripts.paths import list_dir, list_file, check_dir_flask
+from app.scripts.paths import list_dir, list_file, check_dir_flask, check_path
 from app.admin.scripts.login import login_required_custom, access_status
+from app.scripts.sqllite import database
 
 
 @video.route('/video')
@@ -23,9 +28,29 @@ def list_videos(link):
                                typs="show_video")
 
 
-@video.route('/show_video/<path:link>')
+@video.route('/show_video/<path:link>', methods=['GET'])
 @login_required_custom
 @check_dir_flask
 def show_video(link):
     if access_status(5):
         return render_template('video.html', title=link.split('/')[-1], link=link)
+
+
+@video.route('/show_video', methods=['POST'])
+@login_required_custom
+def creat_secret():
+    data = json.loads(request.data)
+    link = data['link'].partition('file/')[2]
+    if check_path(link):
+        username = current_user.username
+        old_secret = database.secret_check(username, link)
+        # 2 hours later
+        if old_secret[2] > time.time() + 79200:
+            secret = old_secret[0]
+        else:
+            database.disable_secret(old_secret[0])
+            secret = database.new_secret(link, username)
+        return jsonify(status=200, url=link+'?secret='+secret), 200
+    else:
+        return jsonify(status=403, text='Access is not allowed'), 200
+
