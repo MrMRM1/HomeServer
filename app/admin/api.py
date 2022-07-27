91f0beebf876126cf70b0319d087710b8b204e89
MrMRM1 <9mehrabani@gmail.com>
2022-07-27 12:38:48 +0430

    add get user information

--- app/admin/api.py
+++ app/admin/api.py
@@ -1,3 +1,6 @@
+from flask import jsonify, request
+from flask_login import current_user
+
 from . import admin
 from .scripts.login import login_required_custom
 from app.scripts.sqllite import database
@@ -8,3 +11,20 @@ from app.admin.scripts.validity_check import check_information
 @login_required_custom
 def register():
     return check_information(database.new_user)
+
+
+@admin.route('/admin/user_information', methods=['POST'])
+@login_required_custom
+def user_information():
+    username_admin = database.get_data()[12]
+    data = request.json
+    if current_user.username == username_admin and username_admin != data['username']:
+        user_data = database.get_user_data(data['username'])
+        if user_data is None:
+            return jsonify(status=404, text="Username is not available"), 200
+        else:
+            return jsonify(status=200, id=user_data[0], paths=user_data[3],
+                           services={"ftp": user_data[4], "video": user_data[5], "audio": user_data[6], "pdf": user_data[7],
+                                     "receive": user_data[8], "send": user_data[9], "system_control": user_data[10],
+                                     "picture": user_data[11]}), 200
+    return jsonify(status=403, text='Access is not allowed'), 200
