0374d5a5afb58af263622f6d654ef85966372c7a
MrMRM1 <9mehrabani@gmail.com>
2022-07-28 13:09:06 +0430

    refactor: add check_status and check path, update check_information

--- app/admin/scripts/validity_check.py
+++ app/admin/scripts/validity_check.py
@@ -22,30 +22,44 @@ def check_password(password: str) -> bool:
         return False
 
 
+def check_status(data):
+    for i in data['services'].keys():
+        if data['services'][i] == '1' or data['services'][i] == '0':
+            continue
+        else:
+            return [i]
+    return True
+
+
+def check_paths(data):
+    path = data['paths']
+    if path is not None:
+        path = path.split(',')
+        for i in path:
+            if check_path(i) is False:
+                return [i]
+        path = ','.join(path)
+    return path
+
+
 def check_information(func):
     username_admin = database.get_data()[12]
     if current_user.username == username_admin:
         data = request.json
         if check_username(data['username']) is False:
-            return jsonify(status=11, test='Username is incorrect'), 200
+            return jsonify(status=11, text='Username is incorrect'), 200
         if data['username'] == username_admin or database.get_user_data(data['username']) is not None:
-            return jsonify(status=12, test='The username is already available'), 200
+            return jsonify(status=12, text='The username is already available'), 200
         if check_password(data['password']) is False:
-            return jsonify(status=13, test='Password is incorrect'), 200
+            return jsonify(status=13, text='Password is incorrect'), 200
         if len(data['services'].keys()) != 8:
-            return jsonify(status=14, test='Services are not complete'), 200
-        for i in data['services'].keys():
-            if data['services'][i] == '1' or data['services'][i] == '0':
-                continue
-            else:
-                return jsonify(status=15, test=f'The status of the {i} service is invalid', service=i), 200
-        path = data['paths']
-        if path is not None:
-            path = path.split(',')
-            for i in path:
-                if check_path(i) is False:
-                    return jsonify(status=16, test='The path is invalid', service=i), 200
-            path = ','.join(path)
+            return jsonify(status=14, text='Services are not complete'), 200
+        status = check_status(data)
+        if isinstance(status, list):
+            return jsonify(status=15, text=f'The status of the {status[0]} service is invalid', problem=status[0]), 200
+        path = check_paths(data)
+        if isinstance(path, list):
+            return jsonify(status=16, text='The path is invalid', problem=path[0]), 200
         if func.__name__ == 'new_user':
             func(data['username'], sha256(data['password'].encode()).hexdigest(), path,
                  [data['services']['ftp'], data['services']['video'], data['services']['audio'], data['services']['pdf'],
