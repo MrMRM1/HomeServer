8034023744a5a3cd31325d80f406e6f1dd78ae27
MrMRM1 <9mehrabani@gmail.com>
2022-07-29 11:23:28 +0430

    feat: add update_access

--- app/admin/api.py
+++ app/admin/api.py
@@ -4,7 +4,7 @@ from flask_login import current_user
 from . import admin
 from .scripts.login import login_required_custom, is_admin
 from app.scripts.sqllite import database
-from app.admin.scripts.validity_check import check_information
+from app.admin.scripts.validity_check import check_information, check_paths, check_status
 
 
 @admin.route('/admin/register', methods=['POST'])
@@ -26,3 +26,18 @@ def user_information():
                        services={"ftp": user_data[4], "video": user_data[5], "audio": user_data[6], "pdf": user_data[7],
                                  "receive": user_data[8], "send": user_data[9], "system_control": user_data[10],
                                  "picture": user_data[11]}), 200
+
+
+@admin.route('/admin/update_access', methods=['POST'])
+@login_required_custom
+@is_admin
+@check_paths
+@check_status
+def update_access():
+    data = request.json
+    database.update_user_information(data['username'], data['paths'], data['services']['ftp'],
+                                     data['services']['video'],
+                                     data['services']['audio'], data['services']['pdf'], data['services']['receive'],
+                                     data['services']['send'], data['services']['system_control'],
+                                     data['services']['picture'])
+    return jsonify(status=200), 200
