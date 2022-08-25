ef5a2542ff55ab9eef2a4eabe2c4beecdbffbd18
MrMRM1 <9mehrabani@gmail.com>
2022-08-25 09:35:33 +0430

    add views

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -3,3 +3,4 @@ from flask import Blueprint
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static', static_url_path='/admin_static')
 
 from . import api
+from . import views
