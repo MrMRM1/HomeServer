c56aa4dd3e00952e0aa9dfe076e828f124ac55b3
MrMRM1 <9mehrabani@gmail.com>
2022-08-28 16:30:04 +0430

    update dashboard add username and password_pattern

--- app/admin/static/js/dashboard.js
+++ app/admin/static/js/dashboard.js
@@ -1,3 +1,6 @@
+let username = [];
+const password_patern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/gm;
+
 function _checkbox(status){
     if (status == '1'){
         return '<td><input type="checkbox" disabled checked></td>'
@@ -14,10 +17,11 @@ fetch('/admin/get_all_users', {
 }).then((jsonObject) => {
     if (jsonObject.status == 200){
         const tbody = document.getElementById('tbody');
-        for (var i in jsonObject.users){
+        for (let i in jsonObject.users){
             tbody.innerHTML += '<tr id="'+ i +'"><th scope="row">' + i +'</th><td>'+ jsonObject.users[i][0] + '</td> </tr>'
-            var tr = document.getElementById(i);
-            for (var j=1; j< jsonObject.users[i].length; j++){
+            username.push(jsonObject.users[i][0]);
+            let tr = document.getElementById(i);
+            for (let j=1; j< jsonObject.users[i].length; j++){
                 tr.innerHTML += _checkbox(jsonObject.users[i][j])
             }
         }
