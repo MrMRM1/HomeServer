b29ee25e976db317df8e5e4f0a51a3d699b7f639
MrMRM1 <9mehrabani@gmail.com>
2022-08-29 11:31:50 +0430

    fix bug update user guest

--- app/admin/api.py
+++ app/admin/api.py
@@ -57,7 +57,7 @@ def update_username():
         return jsonify(status=11, text='Username is incorrect'), 200
     if database.user_data_by_username(data['username']) is not None:
         return jsonify(status=12, text='The username is already available'), 200
-    if data['id'] == 1:
+    if data['id'] == 1 or data['id'] == '1':
         return jsonify(status=20, text='guest username cannot be changed'), 200
     database.write_users_data(data['username'], 'username', data['id'])
     return jsonify(status=200), 200
