8496ec915d85f3570bc308bcdb0b3c72ab7d2410
MrMRM1 <9mehrabani@gmail.com>
2022-08-30 15:43:06 +0430

    update variable update password

--- app/admin/static/js/dashboard.js
+++ app/admin/static/js/dashboard.js
@@ -1,5 +1,5 @@
 let username = [];
-const password_patern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/gm;
+const password_pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/gm;
 const username_pattern = /^(?=.{4,20}$)[a-zA-Z0-9]+$/gm;
 let close_menu = document.getElementById('close_menu');
 
@@ -54,35 +54,33 @@ table_dashboard();
 
 // Update Password 
 
+let update_password_username_select = document.getElementById('username_passwordSelect');
+let update_password_password = document.getElementById('password_change');
+let update_password_password_verification = document.getElementById('password_verification_change');
+
 document.getElementById('update_password_btn').addEventListener("click", () => {
-    let input_username = document.getElementById('username_passwordSelect');
-    let password = document.getElementById('password_change');
-    let password_verification = document.getElementById('password_verification_change');
-    password.value = '';
-    password_verification.value = '';
-    remove_valid_invalid(input_username);
-    remove_valid_invalid(password);
-    remove_valid_invalid(password_verification);
-    input_username.innerHTML = '<option selected>Choose...</option>';
+    update_password_password.value = '';
+    update_password_password_verification.value = '';
+    remove_valid_invalid(update_password_username_select);
+    remove_valid_invalid(update_password_password);
+    remove_valid_invalid(update_password_password_verification);
+    update_password_username_select.innerHTML = '<option selected>Choose...</option>';
     for (let i in username){
-        input_username.innerHTML += '<option value="'+ username[i] + '">' + username[i] +'</option>'
+        update_password_username_select.innerHTML += '<option value="'+ username[i] + '">' + username[i] +'</option>'
     }
 })
 
 document.getElementById('password_save_btn').addEventListener("click", () => {
-    let user_name = document.getElementById('username_passwordSelect');
-    let password = document.getElementById('password_change');
-    let password_verification = document.getElementById('password_verification_change');
     let password_modal_close = document.getElementById('password_modal_close');
-    if (password.value.match(password_patern)){
-        if (password.value == password_verification.value){
-            if (username.includes(user_name.value)){
-                post_data('/admin/update_password', {'username': user_name.value,
-                'password': password.value,
-                'password_verification': password_verification.value}
+    if (update_password_password.value.match(password_patern)){
+        if (update_password_password.value == update_password_password_verification.value){
+            if (username.includes(update_password_username_select.value)){
+                post_data('/admin/update_password', {'username': update_password_username_select.value,
+                'password': update_password_password.value,
+                'password_verification': update_password_password_verification.value}
                 ).then((jsonObject) => {
                     if (jsonObject.status == 200){
-                        showAlert(user_name.value + "'s password has been successfully changed.", 'alert-success')
+                        showAlert(update_password_username_select.value + "'s password has been successfully changed.", 'alert-success')
                     }
                     else {
                         showAlert(jsonObject.text, 'alert-danger')
@@ -93,48 +91,44 @@ document.getElementById('password_save_btn').addEventListener("click", () => {
                 close_menu.click();
             }
             else {
-                user_name.classList.add('is-invalid');
+                update_password_username_select.classList.add('is-invalid');
             }
         }
         else {
-            password_verification.classList.add('is-invalid');
+            update_password_password_verification.classList.add('is-invalid');
         }
     }else {
-        password.classList.add('is-invalid');
+        update_password_password.classList.add('is-invalid');
     }
 })
 
 document.getElementById('password_change').addEventListener('input', () => {
-    let password = document.getElementById('password_change');
-    remove_valid_invalid(password);
-    if (password.value.match(password_patern)){
-        password.classList.add('is-valid');
+    remove_valid_invalid(update_password_password);
+    if (update_password_password.value.match(password_pattern)){
+        update_password_password.classList.add('is-valid');
     }
     else {
-        password.classList.add('is-invalid');
+        update_password_password.classList.add('is-invalid');
     }
 })
 
 document.getElementById('password_verification_change').addEventListener('input', () => {
-    let password_verification_change = document.getElementById('password_verification_change');
-    let password = document.getElementById('password_change');
-    remove_valid_invalid(password_verification_change);
-    if ((password_verification_change.value == password.value) && (password.value.match(password_patern))){
-        password_verification_change.classList.add('is-valid');
+    remove_valid_invalid(update_password_password_verification);
+    if ((update_password_password_verification.value == update_password_password.value) && (update_password_password.value.match(password_patern))){
+        update_password_password_verification.classList.add('is-valid');
     }
     else {
-        password_verification_change.classList.add('is-invalid');
+        update_password_password_verification.classList.add('is-invalid');
     }
 })
 
 document.getElementById('username_passwordSelect').addEventListener('change', () => {
-    let user_name = document.getElementById('username_passwordSelect');
-    remove_valid_invalid(user_name);
-    if (username.includes(user_name.value)){
-        user_name.classList.add('is-valid');
+    remove_valid_invalid(update_password_username_select);
+    if (username.includes(update_password_username_select.value)){
+        update_password_username_select.classList.add('is-valid');
     }
     else {
-        user_name.classList.add('is-invalid');
+        update_password_username_select.classList.add('is-invalid');
     } 
 })
 
