748314c5b75b36450980870d80531b0d1af4d901
MrMRM1 <9mehrabani@gmail.com>
2022-09-14 13:33:26 +0430

    update

--- app/static/js/list_folders.js
+++ app/static/js/list_folders.js
@@ -55,7 +55,7 @@ function click_btn(elmt){
     let temp_root;
     if (typs !== 'dl_file') {
         if (elmt === 'Open%20the%20folder') {
-            url = '/' + typs + '/' + root;
+            url = `/${typs}/${root}`;
             url = url.split('/');
             url.pop(url.length - 1);
             window.location = url.join('/');
@@ -110,7 +110,7 @@ function show_name(elmt){
 function creator(dirs){
     updateHistory(root)
     function text_make (i, download='', href=''){
-        return '<a class="btn ' + color[i % 5] + ' m-2 p-3 rounded-3 col-md-6 d-flex justify-content-between" '+ href +' onclick=click_btn("'+ replace_me(show_name(dirs[i])," ", "%20")+'") ' + download +' ><h1 class="mt-auto mb-auto text-break">' + show_name(dirs[i]) + '</h1></a>'
+        return `<a class="btn ${color[i % 5]}  m-2 p-3 rounded-3 col-md-6 d-flex justify-content-between" ${href} onclick=click_btn("${replace_me(show_name(dirs[i])," ", "%20")}") ${download}><h1 class="mt-auto mb-auto text-break">${show_name(dirs[i]) } </h1></a>`
     }
     const lengh_dir = dirs.length;
     $('#list').remove()
@@ -118,15 +118,15 @@ function creator(dirs){
     if (lengh_dir !== 0){
         $('#main').append('<div id="list" class="col-8 mt-2 ms-auto me-auto"></div>')
         for (let i=0; i < lengh_dir; i = i+2){
-            $('#list').append('<div id="group'+ i +'" class="d-md-flex ms-auto me-auto justify-content-md-around"></div>')
+            $('#list').append(`<div id="group${i}" class="d-md-flex ms-auto me-auto justify-content-md-around"></div>`)
             if (i !== lengh_dir){
 
                 if (typs === 'dl_file'){
                     console.log(root)
-                    $('#group' + i).append(text_make(i, 'download', 'href="/file/'+  window.location.href.split('/all_file/')[1] + '/' + replace_me(dirs[i])+'"'))
+                    $(`#group${i}`).append(text_make(i, 'download', `href="/file/${window.location.href.split('/all_file/')[1]}/${replace_me(dirs[i])}"`))
                 }
                 else {
-                    $('#group' + i).append(text_make(i))
+                    $(`#group${i}`).append(text_make(i))
                 }
 
             }
@@ -134,10 +134,10 @@ function creator(dirs){
 
                 if (typs === 'dl_file'){
 
-                    $('#group' + (i)).append(text_make((i+1), 'download', 'href="/file/'+ window.location.href.split('/all_file/')[1] + '/' +  replace_me(dirs[(i+1)])+'"'));
+                    $(`#group${i}`).append(text_make((i+1), 'download', `href="/file/${window.location.href.split('/all_file/')[1]}/${replace_me(dirs[(i+1)])}"`));
                 }
                 else {
-                    $('#group' + (i)).append(text_make((i+1)));
+                    $(`#group${i}`).append(text_make((i+1)));
                 }
             }
 
