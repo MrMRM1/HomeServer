6748a1a7633d916eda019e48a652538adc2d5211
MrMRM1 <9mehrabani@gmail.com>
2022-09-25 08:41:23 +0330

    add perm_generate for all users

--- app/ftp/__init__.py
+++ app/ftp/__init__.py
@@ -10,29 +10,34 @@ from app.ftp.ftp_scripts.authorizers import DummySha256Authorizer
 from app.scripts.sqllite import database
 
 
-def check_login(data, authorizer):
+def perm_generate(create, store):
     perm = 'elr'
-    if data[8] == '1':
+    if create == '1':
         perm += 'm'
-    if data[9] == '1':
+    if store == '1':
         perm += 'w'
+    return perm
+
+
+def check_login(data, authorizer):
 
     if data[11] == '1':
 
-        authorizer.add_user(data[12], data[13], os.path.realpath(data[7]), perm)
+        authorizer.add_user(data[12], data[13], os.path.realpath(data[7]), perm_generate(data[8], data[9]))
         all_users = database.ftp_users()
         try:
             start = 0
             if data[14] == '1' and all_users[0][0] == 'guest':
                 start = 1
-                authorizer.add_anonymous(homedir=os.path.realpath(all_users[0][2]), perm='elr')
+                authorizer.add_anonymous(homedir=os.path.realpath(all_users[0][2]),
+                                         perm=perm_generate(all_users[0][3], all_users[0][4]))
 
             for i in all_users[start:]:
-                authorizer.add_user(i[0], i[1], os.path.realpath(i[2]))
+                authorizer.add_user(i[0], i[1], os.path.realpath(i[2]), perm=perm_generate(i[3], i[4]))
         except IndexError:
             pass
     else:
-        authorizer.add_anonymous(os.path.realpath(data[7]), perm=perm)
+        authorizer.add_anonymous(os.path.realpath(data[7]), perm=perm_generate(data[8], data[9]))
 
 
 def ftp_server(data, ip):
