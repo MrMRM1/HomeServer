050759aa0eb3473741cf2ceb92a8a7886afae7f3
MrMRM1 <9mehrabani@gmail.com>
2022-11-04 13:43:19 +0330

    add check_update

---
+++ app/scripts/check_update.py
@@ -0,0 +1,19 @@
+from json import loads
+from urllib.request import urlopen, Request
+
+from app.about import UPDATE_NUMBER
+
+
+def check_last_update():
+    output = [False]
+    req = Request(url=f"https://mrmrm.ir/update/Home%20Server.php?v={UPDATE_NUMBER}",
+                  headers={'User-Agent': 'Mozilla/5.0'})
+    data = loads(urlopen(req).read())
+    if data['changes'] == '':
+        return output
+    else:
+        message = "New version available, do you want to download? \nabout this update: \n"
+        changes = data['changes'].split('\\n')
+        message += '\n'.join(changes)
+        output[0] = True
+        return output.append(message)
